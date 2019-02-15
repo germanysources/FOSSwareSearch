7c9aa066e50793b8c5f1703a56ca0cd7cf8ad9c6
Johannes Gerbershagen <johannes.gerbershagen@kabelmail.de>
2019-02-15 02:47:40 +0100

    Prepare for search iwth graph ql

--- SearchGraphQL.py
+++ SearchGraphQL.py
@@ -6,13 +6,23 @@ def execute(cmd_parts):
     print(' '.join(cmd_parts))
     os.system(' '.join(cmd_parts))
 
-execute(['/usr/bin/curl -X POST -H "Content-Type: application/json"',
-         '-H "Authorization: bearer token" -d',
-         '\'{"query":"query { search(query:\\\"'+ sys.argv[1]+'\\\", type:REPOSITORY, first:3) { repositoryCount nodes { ... on Repository {description} }}}"}\'',
+if(len(sys.argv) == 4):
+    execute(['/usr/bin/curl -s --write-out %"{http_code}\n" -X POST -H "Content-Type: application/json"',
+         '-H "Authorization: token '+ sys.argv[2]+' " -d',
+         '\'{"query":"query($search_term:String!,$results:Int!) { search(query:$search_term, type:REPOSITORY, after:\\\"'+sys.argv[3]+'\\\",first:$results) { edges { cursor } nodes { ... on Repository { url }}}}", "variables":{"search_term":"'+sys.argv[1]+'","results":9}}\'',
          
          'https://api.github.com/graphql'
+        
+         ])
+else:
+    execute(['/usr/bin/curl -s --write-out %"{http_code}\n" -X POST -H "Content-Type: application/json"',
+         '-H "Authorization: token '+ sys.argv[2]+' " -d',
+         '\'{"query":"query($search_term:String!,$results:Int!) { search(query:$search_term, type:REPOSITORY, first:$results) { edges { cursor } nodes { ... on Repository {url licenseInfo { key name } stargazers(first:1) { totalCount  } issues(states:OPEN) { totalCount } updatedAt }}}}", "variables":{"search_term":"'+sys.argv[1]+'","results":9}}\'',
          
-])
+         'https://api.github.com/graphql'
+        
+         ])
+    
 
 # Search term for repositories
 SearchRepos = '{"query":"query { search(query:\\\"$search_term:String!\\\", type:REPOSITORY, first:$first_elements:Int){ nodes { ... on Repository{ $fields  } }  }"}'
@@ -30,6 +40,6 @@ def PostQueryViaHttp(PostBody, AuthorizationToken):
     conn.request('POST', '', PostBody, header)
     response = conn.getresponse()
     
-    if(!(response.status >= 200 && response.status < 300)):
+    #if(!(response.status >= 200 && response.status < 300)):
         #not sucessfull
-        raise 
+    #    raise 
